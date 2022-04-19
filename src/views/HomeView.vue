07c3f400a6228e96faeea3d6c36d6811e2796394
mattjkatz <matthewkatz7@gmail.com>
2022-04-19 18:35:02 -0500

    initial commit, create app, install axios, make posts index

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <div v-for="post in posts" v-bind:key="post.id">
+      <p>{{ post }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
